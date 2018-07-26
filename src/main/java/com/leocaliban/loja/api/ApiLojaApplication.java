4b38ffec4af046356a431a4ecde8e8dd0235487a
Leocaliban <leocaliban@gmail.com>
2018-07-26 04:02:03 -0300

    Salva Categoria no banco de dados [via CommandLineRunner]

--- src/main/java/com/leocaliban/loja/api/ApiLojaApplication.java
+++ src/main/java/com/leocaliban/loja/api/ApiLojaApplication.java
@@ -1,12 +1,31 @@
 package com.leocaliban.loja.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leocaliban.loja.api.domain.Categoria;
+import com.leocaliban.loja.api.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ApiLojaApplication {
+public class ApiLojaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Cozinha");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 }
