c15a32daa4c839d59f33dfd4d40855f43c7d41ec
Leocaliban <leocaliban@gmail.com>
2018-07-30 17:39:22 -0300

    Cria o profile de Desenvolvimento

---
+++ src/main/java/com/leocaliban/loja/api/config/DevConfig.java
@@ -0,0 +1,36 @@
+package com.leocaliban.loja.api.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.leocaliban.loja.api.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	// recupera o valor de application-dev.properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	/**
+	 * Só deve inicializar o banco se a estratégia de criação não for create no dev.properties
+	 */
+	@Bean
+	public boolean inicializarBancoDeDados() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.inicializarBancoDeDadosParaTeste();
+		return true;
+	}
+}
