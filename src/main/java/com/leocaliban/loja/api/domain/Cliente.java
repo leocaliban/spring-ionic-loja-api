65c1ce78b38e893920885886ecdfe0246748762a
Leocaliban <leocaliban@gmail.com>
2018-07-29 21:01:20 -0300

    UPDATE: Ajuste na deleção de Cliente

--- src/main/java/com/leocaliban/loja/api/domain/Cliente.java
+++ src/main/java/com/leocaliban/loja/api/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -35,7 +36,7 @@ public class Cliente implements Serializable{
 	//Representação interna feita com Integer ao invés do enum
 	private Integer tipoCliente;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco>enderecos = new ArrayList<>();
 	
 	/*
