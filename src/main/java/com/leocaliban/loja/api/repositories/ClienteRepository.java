ae83ec5479c7a1c1a0d0e16ec24cc97934856781
Leocaliban <leocaliban@gmail.com>
2018-07-30 03:10:40 -0300

    Cria a validação para email duplicado

--- src/main/java/com/leocaliban/loja/api/repositories/ClienteRepository.java
+++ src/main/java/com/leocaliban/loja/api/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.leocaliban.loja.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.leocaliban.loja.api.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	//Indica que a busca não está envolvida com uma transação com o banco
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/leocaliban/loja/api/services/validators/ClienteInsertValidator.java
+++ src/main/java/com/leocaliban/loja/api/services/validators/ClienteInsertValidator.java
@@ -5,12 +5,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.leocaliban.loja.api.domain.Cliente;
 import com.leocaliban.loja.api.domain.enums.TipoCliente;
 import com.leocaliban.loja.api.dto.ClienteNovoDTO;
+import com.leocaliban.loja.api.repositories.ClienteRepository;
 import com.leocaliban.loja.api.resources.exceptions.MensagemDoCampo;
 import com.leocaliban.loja.api.services.validators.utils.CPF_CNPJ;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNovoDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
 	
 	@Override
 	public void initialize(ClienteInsert anotacao) {
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objetoDTO.getTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !CPF_CNPJ.isValidCNPJ(objetoDTO.getCpfOuCnpj())) {
 			lista.add(new MensagemDoCampo("cpfOuCnpj", "CNPJ inválido."));
+		}
+		
+		Cliente clienteComEmail = repository.findByEmail(objetoDTO.getEmail());
+		//Se achar o cliente com email buscado no repositório então o email já existe.
+		if(clienteComEmail != null) {
+			lista.add(new MensagemDoCampo("email", "O E-mail informado já existe."));
 		}
 		
 		//Percorrer a lista para capturar os erros e enviá-las ao validador do framework - método validador do handler.
