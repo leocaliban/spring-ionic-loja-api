84dfe581a4d127fd167be52834a92306f613ec95
Leocaliban <leocaliban@gmail.com>
2018-08-01 05:41:15 -0300

    Configura acesso ao AmazonS3 - Envia primeiro Arquivo

--- src/main/java/com/leocaliban/loja/api/ApiLojaApplication.java
+++ src/main/java/com/leocaliban/loja/api/ApiLojaApplication.java
@@ -1,11 +1,17 @@
 package com.leocaliban.loja.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leocaliban.loja.api.services.S3Service;
+
 @SpringBootApplication
 public class ApiLojaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLojaApplication.class, args);
@@ -13,6 +19,7 @@ public class ApiLojaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.enviarArquivo("F:\\Imagens\\Facebook\\Sem título.png");
 			
 	}
 }
